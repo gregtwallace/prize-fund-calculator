47fd674ee1687dae23c05159e09aaeaaa60c5026
Greg T. Wallace <greg@gregtwallace.com>
2026-04-22 21:34:44 -0400

    install vitest

---
+++ src/App.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest';
+
+// TODO: remove temporary stub so `vite --test` doesn't error
+describe('Temporary: REMOVE ME', () => {
+  it('temp test', () => {
+    expect(true).toBeTruthy();
+  });
+});
